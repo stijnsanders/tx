323c82bfe64545fd79c1b4f2b9db646e37caa396
stijnsanders <devel@yoy.be>
2018-05-16 23:40:02 +0200

    installer upgrade script 1.3.4

--- tx_UPGRADE.sql
+++ tx_UPGRADE.sql
@@ -5,3 +5,19 @@ alter table Rlm add limit_expression varchar(200) null;
 --v1.2.5
 
 -- please see "tx_v1.2.5_collate_nocase.sql"
+
+--v1.3.4
+create table Umi (
+id integer primary key autoincrement,
+usr_id int not null,
+obj_id int not null,
+constraint FK_Umi_Usr foreign key (usr_id) references Usr (id),
+constraint FK_Umi_Obj foreign key (obj_id) references Obj (id)
+);
+create table Umr (
+id integer primary key autoincrement,
+usr_id int not null,
+rpt_id int not null,
+constraint FK_Umr_Usr foreign key (usr_id) references Usr (id),
+constraint FK_Umr_Rpt foreign key (rpt_id) references Rpt (id)
+);
